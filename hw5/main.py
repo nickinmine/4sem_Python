c3b643156769475ac06c67e2e74ed7c961a6cc9b
Nick <nick.a.zubov@gmail.com>
2022-02-24 19:06:35 +0300

    homework 5 dodelat

---
+++ hw5/main.py
@@ -0,0 +1,13 @@
+import math
+
+
+def main():
+    n = 8
+    counter = 0
+    for i in range(1, n+8):
+        counter += 67*math.log(z) ** 4
+    return counter
+
+
+if __name__ == "__main__":
+    print(main())
