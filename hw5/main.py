b08a376faa5e4951614648f7f1823d6880d37ca9
Nick <nickinmine@gmail.com>
2022-02-25 13:57:17 +0300

    hw5

--- hw5/main.py
+++ hw5/main.py
@@ -1,13 +1,14 @@
 import math
 
 
-def main():
-    n = 8
+def main(z):
+    n = z.__len__()
     counter = 0
-    for i in range(1, n+8):
-        counter += 67*math.log(z) ** 4
-    return counter
+    for i in range(1, n+1):
+        counter += (math.log(z[n-math.ceil(i/3)], 10) ** 4)
+    return counter * 67
 
 
 if __name__ == "__main__":
-    print(main())
+    print(main([-0.11, -0.5, -0.55, 0.14, 0.22, 0.64, 0.71, 0.96]))
+    print(main([0.87, -0.15, -0.73, -0.97, 0.17, 0.53, 0.55, 0.55]))
