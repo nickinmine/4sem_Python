6dd3d9c5f9685ebad09c429ffee4ebdc5c6eb6dd
Nick <nickinmine@gmail.com>
2022-03-22 12:07:03 +0300

    hw8 droch

--- hw8/main.py
+++ hw8/main.py
@@ -7,14 +7,14 @@ def main1(arg):
     temp = arg.split('\0')
     for i in range(len(temp)):
         temp[i] = temp[i].strip()
-    arg = arg.replace('\0', '\n').replace('"', '\u0027')
-    arg = arg.replace('\u0027[', '\u0027: [').replace('\u0027 [', '\u0027: [')
-    arg = arg.replace(' ]', '\u0027]')
-    arg = arg.replace('[', '[\u0027').replace('[\u0027 ', '[\u0027')
-    arg = arg.replace('; ', '\u0027, \u0027').replace(' ;', '\u0027, \u0027')\
-        .replace(';', '\u0027, \u0027')
+    arg = arg.replace('\0', '\n').replace('"', '\'')
+    arg = arg.replace('\'[', '\': [').replace('\' [', '\': [')
+    arg = arg.replace(' ]', '\']')
+    arg = arg.replace('[', '[\'').replace('[\' ', '[\'')
+    arg = arg.replace('; ', '\', \'').replace(' ;', '\', \'')\
+        .replace(';', '\', \'')
     arg = arg.replace(' ', '').replace(':', ": ")\
-        .replace(',\u0027', ', \u0027')
+        .replace(',\'', ', \'')
     arg = arg.replace('\n', '\n ')
     #arg = arg.replace('"', '')
 
@@ -33,7 +33,7 @@ def main(temp):
             'atge_380': ['ongele', 'eserus_69', 'onrate_619']}
     res4 = {'tesora_738': ['tius', 'vedi', 'enes', 'socege_395'],
             'arma_658': ['estete', 'erat', 'erge_438']}
-    res5 = {'lalaar_468': ['oren', 'enes', 'ordive_574', 'erve']
+    res5 = {'lalaar_468': ['oren', 'enes', 'ordive_574', 'erve'],
             'aron': ['legeed_234', 'geisso_826'],
             'tier': ['cequ', 'erteis', 'iseddi', 'reen_478'],
             'onis_739': ['teatus_605', 'beve']}
@@ -99,6 +99,7 @@ def init(input):
     for s in rawSplit2:
         result[s[0]] = s[1]
     return result
+
 if __name__ == "__main__":
     #print(main1('<section> .begin var "inorso" [ora_848;dibe_131 ];.end, .begin var "isgela"[ orreus_974 ; rige ;ataen_512 ;eran ];.end, .begin var "tibeve_820"[orxema ; anor_495 ]; .end,</section>'))
     print()
@@ -107,10 +108,10 @@ if __name__ == "__main__":
     print(main1('<section>.begin var"lalaar_468"[oren; enes; ordive_574 ; erve ];.end, .begin var "aron"[ legeed_234 ; geisso_826]; .end, .beginvar"tier"[ cequ ;erteis; iseddi; reen_478 ]; .end, .begin var"onis_739" [ teatus_605 ;beve ]; .end, </section>'))
     #print(init('.do << let cedi_433::= array( #3455 .#3895 .#-9958 . #6714 ) >>        <<let usteis ::=array( #-7917 . #3935 . #-3181) >>.end'))
     print()
-    print(main('<section> .begin var "inorso" [ora_848;dibe_131 ];.end, .begin var\n"isgela"[ orreus_974 ; rige ;ataen_512 ;eran ];.end, .begin var\n"tibeve_820"[orxema ; anor_495 ]; .end,</section>'))
-    print(main('<section>.begin var "belece_775" [ anve ; atre_268 ;laa_129 ; anbe];\n.end, .begin var "qumaer"[ atarte ; usrion_138 ; leed; orge ];\n.end,.begin var "onle" [ xeardi_266 ; zaveza ; dianis_330 ]; .end,\n</section>'))
-    print(main('<section>.begin var "mais" [ dixein_577; isisbi_456 ; bedi ; edus_9];\n.end, .begin var "isriso_836" [ laat ; usedor ; onar]; .end, .begin\nvar "raxe_832"[ islege_265 ; usbe_490]; .end, .begin var "atge_380" [\nongele ; eserus_69 ; onrate_619 ]; .end,</section>'))
-    print(main('<section>.begin var "tesora_738" [ tius; vedi; enes ;socege_395 ];\n.end,.begin var "arma_658" [ estete ; erat; erge_438];.end, </section>'))
-    print(main('<section>.begin var"lalaar_468"[oren; enes; ordive_574 ; erve ];\n.end, .begin var "aron"[ legeed_234 ; geisso_826]; .end, .begin\nvar"tier"[ cequ ;erteis; iseddi; reen_478 ]; .end, .begin var\n"onis_739" [ teatus_605 ;beve ]; .end, </section>'))
+    #print(main('<section> .begin var "inorso" [ora_848;dibe_131 ];.end, .begin var\n"isgela"[ orreus_974 ; rige ;ataen_512 ;eran ];.end, .begin var\n"tibeve_820"[orxema ; anor_495 ]; .end,</section>'))
+    #print(main('<section>.begin var "belece_775" [ anve ; atre_268 ;laa_129 ; anbe];\n.end, .begin var "qumaer"[ atarte ; usrion_138 ; leed; orge ];\n.end,.begin var "onle" [ xeardi_266 ; zaveza ; dianis_330 ]; .end,\n</section>'))
+    #print(main('<section>.begin var "mais" [ dixein_577; isisbi_456 ; bedi ; edus_9];\n.end, .begin var "isriso_836" [ laat ; usedor ; onar]; .end, .begin\nvar "raxe_832"[ islege_265 ; usbe_490]; .end, .begin var "atge_380" [\nongele ; eserus_69 ; onrate_619 ]; .end,</section>'))
+    #print(main('<section>.begin var "tesora_738" [ tius; vedi; enes ;socege_395 ];\n.end,.begin var "arma_658" [ estete ; erat; erge_438];.end, </section>'))
+    #print(main('<section>.begin var"lalaar_468"[oren; enes; ordive_574 ; erve ];\n.end, .begin var "aron"[ legeed_234 ; geisso_826]; .end, .begin\nvar"tier"[ cequ ;erteis; iseddi; reen_478 ]; .end, .begin var\n"onis_739" [ teatus_605 ;beve ]; .end, </section>'))
 
 
